2fae39df206127cdcebe47ba49339c2fb5f05b67
horacda2 <dan.horajda@gmail.com>
2024-03-20 13:39:53 +0100

    Rozšíření třídy Hlavni

--- src/main/java/Trida.java
+++ src/main/java/Trida.java
@@ -1,9 +1,8 @@
-/**
- * trida
- */
-/
+/*
+* hlavni trida velkeho projektu*/
 public class Trida {
     public static void main(String[] args) {
         System.out.println("Třída třída.");
+        System.out.println("Pridávno v klonu");
     }
 }
